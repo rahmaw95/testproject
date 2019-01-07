e8431093980d23323d9e8dcf038a72b1885ff969
Siti Rahmawaty <sty.rahmawati@gmail.com>
2019-01-07 18:30:48 +0700

    update scss

---
+++ src/components/common/Banner/Banner.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="banner bgColor:primary">
+    <p class="fontColor:white">Welcome</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
